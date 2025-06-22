0f87cadd7ddadff86f62a8aed1adf16d5ad138f2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-06-23 02:47:03 +0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,16 @@
+import numpy as np
+array1=np.zeros(10)
+array1[5] = 100
+
+
+array2=np.ones((3,3))
+array2=array2*7
+
+array3=np.arange(10,50,5)
+
+array4=np.full((2,4),9 )
+
+array5=np.eye((4,4),dtype=int)
+array7=np.linspace(0,1,6,endpoint=False)
+
+array8=np.random.randint(1,100,5) 
